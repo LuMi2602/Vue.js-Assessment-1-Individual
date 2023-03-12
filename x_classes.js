7698fa987d57ef6d2af5fd19894a9d9fbe7b0d5e
LuMi2602 <wedimulie@gmail.com>
2023-03-12 17:29:19 +0400

    Commit 1

---
+++ x_classes.js
@@ -0,0 +1,120 @@
+let classes  = [   
+    {
+    "id": 1,
+    "Image": "Images_For_Subjects\Coursework1_french.jpg",
+    "Subject":"French", 
+    "Location": "Room 105",
+    "price": 500 + " AED",
+    "Space": 5
+    
+       }, 
+    
+    
+    Art
+    Music
+    Creative writing
+    Football 
+    BasketBall
+    Dancing
+    Acting
+    Singing
+    
+
+    {
+        "id": 2,
+        "Image": "Images_For_Subjects\Coursework1_french.jpg",
+        "Subject":"Coding", 
+        "Location": "Room 107",
+        "price": 700 + " AED",
+        "Space": 5     
+    }, 
+
+        {
+           
+           "id": 1003,
+            "title": "Cat Food, 25lb bag",
+           "description": 
+           "A 25 pound bag of irresistible,organic goodness for your cat.",
+           "price": 2000,
+           "image": "assets/images/product-fullsize.png",
+           "availableInventory": 10,
+           "rating": 2
+        },
+        {
+           
+           "id": 1004,
+            "title": "Cat Food, 25lb bag",
+           "description": 
+           "A 25 pound bag of irresistible,organic goodness for your cat.",
+           "price": 2000,
+           "image": "assets/images/product-fullsize.png",
+           "availableInventory": 10,
+           "rating": 2
+        },
+        {
+           
+           "id": 1005,
+           "title": "Cat Food, 25lb bag",
+           "description": 
+           "A 25 pound bag of irresistible,organic goodness for your cat.",
+           "price": 2000,
+           "image": "assets/images/product-fullsize.png",
+           "availableInventory": 10,
+           "rating": 2
+        }, 
+        {
+           
+            "id": 1005,
+            "title": "Cat Food, 25lb bag",
+            "description": 
+            "A 25 pound bag of irresistible,organic goodness for your cat.",
+            "price": 2000,
+            "image": "assets/images/product-fullsize.png",
+            "availableInventory": 10,
+            "rating": 2
+         },
+         {
+           
+            "id": 1005,
+            "title": "Cat Food, 25lb bag",
+            "description": 
+            "A 25 pound bag of irresistible,organic goodness for your cat.",
+            "price": 2000,
+            "image": "assets/images/product-fullsize.png",
+            "availableInventory": 10,
+            "rating": 2
+         },
+         {
+           
+            "id": 1005,
+            "title": "Cat Food, 25lb bag",
+            "description": 
+            "A 25 pound bag of irresistible,organic goodness for your cat.",
+            "price": 2000,
+            "image": "assets/images/product-fullsize.png",
+            "availableInventory": 10,
+            "rating": 2
+         },
+         {
+           
+            "id": 1005,
+            "title": "Cat Food, 25lb bag",
+            "description": 
+            "A 25 pound bag of irresistible,organic goodness for your cat.",
+            "price": 2000,
+            "image": "assets/images/product-fullsize.png",
+            "availableInventory": 10,
+            "rating": 2
+         },
+         {
+           
+            "id": 1005,
+            "title": "Cat Food, 25lb bag",
+            "description": 
+            "A 25 pound bag of irresistible,organic goodness for your cat.",
+            "price": 2000,
+            "image": "assets/images/product-fullsize.png",
+            "availableInventory": 10,
+            "rating": 2
+         },
+        ]
