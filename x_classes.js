172f3f07b923e80111fa4b4b77e50e107dc5bd72
LuMi2602 <wedimulie@gmail.com>
2023-03-14 17:41:26 +0400

    update 2

--- x_classes.js
+++ x_classes.js
@@ -1,30 +1,20 @@
-let classes  = [   
+let x_classes  = [   
     {
     "id": 1,
-    "Image": "Images_For_Subjects\Coursework1_french.jpg",
+    "Image": "assets\Images_For_Subjects\Coursework1_french.jpg",
     "Subject":"French", 
     "Location": "Room 105",
-    "price": 500 + " AED",
+    "Price": 500 + " AED",
     "Space": 5
-    
+
        }, 
     
-    
-    
-    
-    
-    
-    
-    
-   
-    
-
     {
         "id": 2,
         "Image": "Images_For_Subjects\Coursework1_french.jpg",
         "Subject":"Coding", 
         "Location": "Room 107",
-        "price": 700 + " AED",
+        "Price": 700 + " AED",
         "Space": 5     
     }, 
 
@@ -33,7 +23,7 @@ let classes  = [
       "Image": "Images_For_Subjects\Coursework1_french.jpg",
       "Subject":"Art", 
       "Location": "Room 110",
-      "price": 750 + " AED",
+      "Price": 750 + " AED",
       "Space": 5     
   }, 
 
@@ -42,7 +32,7 @@ let classes  = [
    "Image": "Images_For_Subjects\Coursework1_french.jpg",
    "Subject":"Music", 
    "Location": "Music Room",
-   "price": 600 + " AED",
+   "Price": 600 + " AED",
    "Space": 5     
       }, 
 
@@ -51,7 +41,7 @@ let classes  = [
          "Image": "Images_For_Subjects\Coursework1_french.jpg",
          "Subject":"Creative Writing", 
          "Location": "Room 021",
-         "price": 600 + " AED",
+         "Price": 600 + " AED",
          "Space": 5     
             }, 
 
@@ -61,7 +51,7 @@ let classes  = [
             "Image": "Images_For_Subjects\Coursework1_french.jpg",
             "Subject":"Football ", 
             "Location": "Football Field",
-            "price": 600 + " AED",
+            "Price": 600 + " AED",
             "Space": 5     
                }, 
          
@@ -71,7 +61,7 @@ let classes  = [
                   "Image": "Images_For_Subjects\Coursework1_french.jpg",
                   "Subject":"Dancing", 
                   "Location": "Main Hall",
-                  "price": 800 + " AED",
+                  "Price": 800 + " AED",
                   "Space": 5     
              },  
              
@@ -80,7 +70,7 @@ let classes  = [
                "Image": "Images_For_Subjects\Coursework1_french.jpg",
                "Subject":"Acting", 
                "Location": "School Theatre",
-               "price": 1000 + " AED",
+               "Price": 1000 + " AED",
                "Space": 5     
           }, 
           {
@@ -88,7 +78,7 @@ let classes  = [
             "Image": "Images_For_Subjects\Coursework1_french.jpg",
             "Subject":"Singing", 
             "Location": "Room 220",
-            "price": 800 + " AED",
+            "Price": 800 + " AED",
             "Space": 5     
        }, 
         ]
