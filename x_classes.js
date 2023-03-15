98850da1a52849fe8e105fc5d19ec1e1d4efb923
LuMi2602 <wedimulie@gmail.com>
2023-03-15 21:10:45 +0400

    update 7

--- x_classes.js
+++ x_classes.js
@@ -1,7 +1,7 @@
 let x_classes  = [   
     {
     "id": 1,
-    "Image": "Images_For_Subjects\Coursework1_french.jpg",
+    "Image": "Images_For_Subjects/french.jpg",
     "Subject":"French", 
     "Location": "Room 105",
     "Price": 800 + " AED",
@@ -11,7 +11,7 @@ let x_classes  = [
     
     {
         "id": 2,
-        "Image": "Images_For_Subjects\Coursework1_french.jpg",
+        "Image": "Images_For_Subjects/coding.jpg",
         "Subject":"Coding", 
         "Location": "Room 107",
         "Price": 700 + " AED",
@@ -20,7 +20,7 @@ let x_classes  = [
 
     {
       "id": 3,
-      "Image": "Images_For_Subjects\Coursework1_french.jpg",
+      "Image": "Images_For_Subjects/art.jpg",
       "Subject":"Art", 
       "Location": "Room 110",
       "Price": 750 + " AED",
@@ -29,7 +29,7 @@ let x_classes  = [
 
    {
    "id": 4,
-   "Image": "Images_For_Subjects\Coursework1_french.jpg",
+   "Image": "Images_For_Subjects/music.jpg",
    "Subject":"Music", 
    "Location": "Music Room",
    "Price": 600 + " AED",
@@ -38,7 +38,7 @@ let x_classes  = [
 
       {
          "id": 5,
-         "Image": "Images_For_Subjects\Coursework1_french.jpg",
+         "Image": "Images_For_Subjects/creative_writing.jpg",
          "Subject":"Creative Writing", 
          "Location": "Room 021",
          "Price": 600 + " AED",
@@ -48,7 +48,7 @@ let x_classes  = [
         
          {
             "id": 6,
-            "Image": "Images_For_Subjects\Coursework1_french.jpg",
+            "Image": "Images_For_Subjects/football.jpg",
             "Subject":"Football ", 
             "Location": "Football Field",
             "Price": 600 + " AED",
@@ -58,7 +58,7 @@ let x_classes  = [
      
                {
                   "id": 7,
-                  "Image": "Images_For_Subjects\Coursework1_french.jpg",
+                  "Image": "Images_For_Subjects/dancing.jpg",
                   "Subject":"Dancing", 
                   "Location": "Main Hall",
                   "Price": 800 + " AED",
@@ -67,7 +67,7 @@ let x_classes  = [
 
              {
                "id": 8,
-               "Image": "Images_For_Subjects/Coursework1_french.jpg",
+               "Image": "Images_For_Subjects/basketball.jpg",
                "Subject":"Basketball", 
                "Location": "Basketball Court",
                "Price": 600 + " AED",
@@ -76,7 +76,7 @@ let x_classes  = [
              
              {
                "id": 9,
-               "Image": "Images_For_Subjects\Coursework1_french.jpg",
+               "Image": "Images_For_Subjects/acting.jpg",
                "Subject":"Acting", 
                "Location": "School Theatre",
                "Price": 600 + " AED",
@@ -84,7 +84,7 @@ let x_classes  = [
           }, 
           {
             "id": 10,
-            "Image": "Images_For_Subjects\Coursework1_french.jpg",
+            "Image": "Images_For_Subjects/singing.jpg",
             "Subject":"Singing", 
             "Location": "Room 220",
             "Price": 800 + " AED",
